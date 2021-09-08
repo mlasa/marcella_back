a42218c584b9a9a496a8d42bcedb14a1d8f86011
Marcella Amorim <marcella.amorimsa@gmail.com>
2021-09-07 22:33:44 -0300

    Procfile

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectToDB from './utils/connectToDB'
 import router from './routes/index'
+import cors from 'cors'
 
 dotenv.config()
 
+
 const app = express()
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
